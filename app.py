d11aeb32aeedbf90a58d76d0c5bb986346e3ab06
avideshh98-source <avideshh98@gmail.com>
2026-05-30 16:36:46 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 # ---------------- GEMINI SETUP ----------------
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-pro")
 
 st.title("📊 AI Crypto Scalp Tool (Gemini AI)")
 
